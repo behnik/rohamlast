e602c337de752df3bd8d03062ade8ef4e7988b09
علی به نیک <ali.behnik@outlook.com>
2024-11-05 05:40:31 +0000

    add stimulsoft

---
+++ Roham.Server/public/stimulsoft-reports-js/Scripts/stimulsoft.viewer.d.ts
@@ -0,0 +1,10 @@
+/*
+Stimulsoft.Reports.JS
+Version: 2023.1.1
+Build date: 2022.12.07
+License: https://www.stimulsoft.com/en/licensing/reports
+*/
+export * from './stimulsoft.dashboards';
+import {Stimulsoft} from './stimulsoft.dashboards';
+export import StiViewer = Stimulsoft.Viewer.StiViewer;
+export import StiViewerOptions = Stimulsoft.Viewer.StiViewerOptions;
